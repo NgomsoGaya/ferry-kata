87fdc4f6c6e92558d64c498ad1c2ef99869573bd
Ngomso Gaya <125261321+NgomsoGaya@users.noreply.github.com>
2025-05-09 12:56:26 +0200

    intializing braze sdk

--- FabrikHunter/backend/src/config/database.js
+++ FabrikHunter/backend/src/config/database.js
@@ -6,4 +6,4 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://fabrikhunter.onrender.com/', credentials: true }));
